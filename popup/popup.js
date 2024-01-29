11bb8afca721e857b9274dda877426d190bff7b7
dvnshi <devanshijain460@gmail.com>
2024-01-29 11:05:41 +0530

    added report button

--- popup/popup.js
+++ popup/popup.js
@@ -699,3 +699,12 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Add an event listener to the Report button
+    document.getElementById("reportButton").addEventListener("click", function () {
+        // Open a new tab with the specified link
+        chrome.tabs.create({ url: "https://dapde.de/en/publikationen-co-en/dark-pattern-melden_de-en/" });
+    });
+});
+
